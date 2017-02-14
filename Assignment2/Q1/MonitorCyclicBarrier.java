b50666e97b5ebda64f9bd5e5d96f7840f51701a0
sbordov <stefan.bordovsky@gmail.com>
2017-02-14 01:07:10 -0600

    Seems to be in working order!

--- Assignment2/Q1/MonitorCyclicBarrier.java
+++ Assignment2/Q1/MonitorCyclicBarrier.java
@@ -15,17 +15,24 @@ public class MonitorCyclicBarrier {
     }
 
     public int await() throws InterruptedException {
-       int index = getIndex();
-       while(index > 0){
-           try{
-               wait();
-           } catch(InterruptedException e){
-               
+       int index = getIndex(); // Sleeps threads not in the current round.
+       
+       synchronized(this){
+           while(index > 0){
+               try{
+                   wait();
+                   break;
+               } catch(InterruptedException e){
+
+               }
+           }
+           if(index == 0){
+               this.notifyAll();
            }
        }
-       notifyAll();
+       resetIndex();
        // Threads begin entering. getIndex();
-       // Threads hit wait until barrier_index = parties.
+       // Threads hit wait until barrier_index = 0.
        // thread[parties - 1] notifies parties - 1 threads to proceed.
        // After all threads are gone, start letting num_parties other threads wait at barrier.
        
@@ -41,12 +48,14 @@ public class MonitorCyclicBarrier {
                 
             }
         }
-        notify();
+        //notifyAll();
         return barrier_index--;
     }
     
     public synchronized void resetIndex(){
-        barrier_index = num_parties - 1;
-        notifyAll();
+        barrier_index++;
+        if(barrier_index == num_parties - 1){
+            notifyAll();
+        }
     }
 }
