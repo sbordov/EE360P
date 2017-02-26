79a267332bff61830dbada8732268685c16c2483
sbordov <stefan.bordovsky@gmail.com>
2017-02-25 22:40:09 -0600

    Added inventory parsing for Server and began creating skeleton code for Client.

--- Assignment3/Q3/Client.java
+++ Assignment3/Q3/Client.java
@@ -1,47 +1,92 @@
 import java.util.Scanner;
 
-public class Client { // Looks like I can edit.
-  public static void main (String[] args) {
-    String hostAddress;
-    int tcpPort;
-    int udpPort;
-
-    if (args.length != 3) {
-      System.out.println("ERROR: Provide 3 arguments");
-      System.out.println("\t(1) <hostAddress>: the address of the server");
-      System.out.println("\t(2) <tcpPort>: the port number for TCP connection");
-      System.out.println("\t(3) <udpPort>: the port number for UDP connection");
-      System.exit(-1);
+public class Client {
+    private static Protocol protocol;
+    
+    public enum Protocol {
+        TCP, UDP
     }
+    
+    public static void main (String[] args) {
+        String hostAddress;
+        int tcpPort;
+        int udpPort;
 
-    hostAddress = args[0];
-    tcpPort = Integer.parseInt(args[1]);
-    udpPort = Integer.parseInt(args[2]);
+        if (args.length != 3) {
+            System.out.println("ERROR: Provide 3 arguments");
+            System.out.println("\t(1) <hostAddress>: the address of the server");
+            System.out.println("\t(2) <tcpPort>: the port number for TCP connection");
+            System.out.println("\t(3) <udpPort>: the port number for UDP connection");
+            System.exit(-1);
+        }
 
-    Scanner sc = new Scanner(System.in);
-    while(sc.hasNextLine()) {
-      String cmd = sc.nextLine();
-      String[] tokens = cmd.split(" ");
+        hostAddress = args[0];
+        tcpPort = Integer.parseInt(args[1]);
+        udpPort = Integer.parseInt(args[2]);
 
-      if (tokens[0].equals("setmode")) {
-        // TODO: set the mode of communication for sending commands to the server 
-        // and display the name of the protocol that will be used in future
-      }
-      else if (tokens[0].equals("purchase")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("cancel")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("search")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("list")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else {
-        System.out.println("ERROR: No such command");
-      }
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()) {
+            String cmd = sc.nextLine();
+            String[] tokens = cmd.split(" ");
+
+            if (tokens[0].equals("setmode")) {
+                // TODO: set the mode of communication for sending commands to the server 
+                // and display the name of the protocol that will be used in future
+                setMode(tokens[1]);
+            }
+            else if (tokens[0].equals("purchase")) {
+                // TODO: send appropriate command to the server and display the
+                // appropriate responses form the server
+                processPurchase(tokens);
+            } else if (tokens[0].equals("cancel")) {
+                // TODO: send appropriate command to the server and display the
+                // appropriate responses form the server
+                processCancel(tokens);
+            } else if (tokens[0].equals("search")) {
+                // TODO: send appropriate command to the server and display the
+                // appropriate responses form the server
+                processSearch(tokens);
+            } else if (tokens[0].equals("list")) {
+                // TODO: send appropriate command to the server and display the
+                // appropriate responses form the server
+                processList(tokens);
+            } else {
+                System.out.println("ERROR: No such command");
+            }
+        }
     }
-  }
+  
+    public static void setMode(String input){
+        if(input.equalsIgnoreCase("t")){
+            protocol = Protocol.TCP;
+        } else if(input.equalsIgnoreCase("u")){
+            protocol = Protocol.UDP;
+        }
+    }
+    
+    public static void processPurchase(String[] input){
+        if(input.length!= 4){
+            invalidInputWarning();
+            return;
+        }
+        
+        
+    }
+    
+    public static void processCancel(String[] input){
+        
+    }
+    
+    public static void processSearch(String[] input){
+        
+    }
+    
+    public static void processList(String[] input){
+        
+    }
+    
+    public static void invalidInputWarning(){
+        System.out.println("ERROR: Invalid input.");
+    }
+    
 }
--- Assignment3/Q3/Server.java
+++ Assignment3/Q3/Server.java
@@ -1,4 +1,15 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Server {
+    private static HashMap<String, Integer> inventory = new HashMap<>();
+    
   public static void main (String[] args) {
     int tcpPort;
     int udpPort;
@@ -14,8 +25,45 @@ public class Server {
     udpPort = Integer.parseInt(args[1]);
     String fileName = args[2];
 
-    // parse the inventory file
+    // Parse the inventory file
+    parseInventory();
+    
 
     // TODO: handle request from clients
+  }
+  
+  public static void parseInventory(){
+      try(BufferedReader br = new BufferedReader(new FileReader("file.txt"))) {
+        StringBuilder sb = new StringBuilder();
+        String line = br.readLine();
+
+        while (line != null) {
+            stockItem(line);
+            sb.append(line);
+            sb.append(System.lineSeparator());
+            line = br.readLine();
+        }
+        String fileText = sb.toString();
+    } catch (IOException ex) {
+        Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+  
+  public static void stockItem(String input){
+    String[] tokens = input.split("\\s+");
+    if(tokens.length != 2){
+        System.out.println("ERROR: Invalid inventory input.");
+        return;
+    }
+    String item_name = tokens[0];
+    int number_in_stock;
+    try{
+        number_in_stock = Integer.parseInt(tokens[1]);
+    } catch(NumberFormatException e){
+        number_in_stock = -1;
+    }
+    if(number_in_stock != -1){
+        inventory.put(item_name, number_in_stock);
+    }
   }
 }
